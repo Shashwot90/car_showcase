c4d72289b52a9dbf63abec8500f3df99299970eb
Shashwot Pradhan <shashwotpradhan@gmail.com>
2023-07-20 09:48:29 +0545

    server side rendering

--- utils/index.ts
+++ utils/index.ts
@@ -1,11 +1,13 @@
-import { CarProps } from "@/types";
+import { CarProps, FilterProps } from "@/types";
 
-export async function fetchCars() {
+export async function fetchCars(filters: FilterProps) {
+  const { manufacturer, year, model, limit, fuel } = filters;
+
     const headers = {
 		'X-RapidAPI-Key': '43e9e371cfmshbc3fc2a1afeaf39p134a77jsna5e15b538483',
 		'X-RapidAPI-Host': 'cars-by-api-ninjas.p.rapidapi.com'
 	}
-    const response = await fetch('https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?model=corolla', {headers: headers,});
+    const response = await fetch(`https://cars-by-api-ninjas.p.rapidapi.com/v1/cars?make=${manufacturer}&year=${year}&model=${model}&limit=${limit}&fuel_type=${fuel}`, {headers: headers,});
     const result = await response.json();
     return result;
 }
